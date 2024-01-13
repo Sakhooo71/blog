ab364ddd1ee4b83c7484ec4b7400a9cbf27da56a
hamady <sakhooo71.com>
2024-01-13 03:15:34 +0100

    creations post et articles

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,24 +6,30 @@ use App\Entity\Post;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-         $post = new Post();
-         $post->setTitle('Tintin au tibet')
-              ->setSlug('tintin-au-tibet')
-              ->setContent('Lorem ipsum')
-              ->setImage('https://via.placeholder.com/350x150')
-              ->setCreatedAt(new DateTimeImmutable('now'))
-              ->setUpdatedAt(new DateTimeImmutable('now'))
-              ;
+        $faker = Factory::create('fr_FR'); // Chargement de Faker en français
 
+        for ($i = 0; $i < 50; $i++) { // Boucles pour 50 articles
 
+            $post = new Post();
+            $post->setTitle($faker->sentence())
+                 ->setSlug($faker->slug())
+                 ->setContent($faker->paragraphs(5, true))
+                 ->setImage('https://via.placeholder.com/350*150') // Generate a random image URL
+                 ->setCreatedAt(new DateTimeImmutable('1 month ago')) // Use Faker for dates
+                 ->setUpdatedAt(new DateTimeImmutable('1 week ago'))
+            ;
 
-         $manager->persist($post);
+            $manager->persist($post);
+        };
 
         $manager->flush();
     }
 }
+
+
